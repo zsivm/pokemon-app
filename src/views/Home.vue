99f4332972253aec712403484367a86907fca93f
Mihael Zsivics <zsivics.mihael@gmail.com>
2021-07-04 19:10:45 +0200

    creted simple pokemon fetch app with pokemonapi, using vue js 3 and composition api based on this tutorial: https://www.youtube.com/watch?v=QJhqr7jqxVo

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,50 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="w-full flex justify-center">
+    <input v-model="text" type="text" placeholder="Enter Pokemon" class="mt-10 p-2 border-blue-500 border-2">
+  </div>
+  <div class="mt-10 p-4 flex flex-wrap justify-center">
+    <div class="ml-4 text-2xl text-blue-400"
+      v-for="(pokemon, idx) in filteredPokemon" :key="idx"
+    >
+      <router-link :to="`/about/${urlIdLookup[pokemon.name]}`">
+        {{pokemon.name}}
+      </router-link>
+    </div>
+
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
+import { reactive, toRefs, computed } from "vue"
 export default {
   name: 'Home',
-  components: {
-    HelloWorld
+  setup() {
+
+    const state = reactive({
+      pokemons: [],
+      urlIdLookup: {},
+      text: "",
+      filteredPokemon: computed(() => updatePokemon())
+    })
+
+    function updatePokemon() {
+      if(!state.text) {
+        return [];
+      }
+
+      return state.pokemons.filter((pokemon) => pokemon.name.includes(state.text));
+    }
+
+    fetch("https://pokeapi.co/api/v2/pokemon?offset=0")
+      .then(res => res.json())
+      .then(data => {
+        state.pokemons = data.results;
+        state.urlIdLookup = data.results.reduce((acc, cur, idx) => 
+        acc = {...acc, [cur.name]:idx + 1}
+        , {})
+      }) 
+
+      return { ... toRefs(state)}
   }
 }
 </script>
